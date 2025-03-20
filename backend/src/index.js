1ffd637ec6a1d0d2d890c8f0787b76c9c2f145d5
AKASH <akash.pradeep@appsteamtechnologies.com>
2025-03-20 18:49:34 +0530

    cors connection

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import messageRoutes from "./routes/message.route.js";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { connectDB } from "./lib/db.js";
+import cors from "cors";
 
 dotenv.config();
 
@@ -14,6 +15,13 @@ const app = express();
 app.use(express.json());
 
 app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRoutes);
 
