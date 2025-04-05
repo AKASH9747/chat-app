9d266192e4b53cc2866572fe5380a2f3c1ab544e
AKASH <akash.pradeep@appsteamtechnologies.com>
2025-04-05 13:20:19 +0530

    Feat :signUP,login,profile,settings - done

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -21,7 +21,7 @@ const SignUpPage = () => {
     password: "",
   });
 
-  const { signup, isSigningUp } = useAuthStore();
+  const { signUp, isSigningUp } = useAuthStore();
 
   const validateForm = () => {
     if (!formData.fullName.trim()) return toast.error("Full name is required");
@@ -40,7 +40,7 @@ const SignUpPage = () => {
 
     const success = validateForm();
 
-    if (success === true) signup(formData);
+    if (success === true) signUp(formData);
   };
 
   return (
