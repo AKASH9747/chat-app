be10c052535a004d17d47e835a49ed2cb3129e2b
AKASH <akash.pradeep@appsteamtechnologies.com>
2025-04-30 19:10:45 +0530

    Feat: chat container desing and functionalities - on going

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import { useChatStore } from "../store/useChatStore";
 import Sidebar from "../components/Sidebar";
 import NoChatSelected from "../components/NoChatSelected";
+import ChatContainer from "../components/ChatContainer";
 
 const HomePage = () => {
   const { selectedUser } = useChatStore();
+  console.log("🚀 ~ HomePage ~ selectedUser:", selectedUser);
 
   return (
     <div className="h-screen bg-base-200">
